3b71aaf3b3cfca7e69b370f69f7e086ab1bda428
pablovvoliveira <pablovvoliveira@gmail.com>
2023-06-20 11:51:19 -0300

    Hello World

--- src/main/java/com/pablovvoliveira/vendasapi/VendasApiApplication.java
+++ src/main/java/com/pablovvoliveira/vendasapi/VendasApiApplication.java
@@ -2,10 +2,18 @@ package com.pablovvoliveira.vendasapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApiApplication {
 
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApiApplication.class, args);
 	}
