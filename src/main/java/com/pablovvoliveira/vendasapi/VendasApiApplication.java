875a6c420abff52bc8ea342cffe8f45f0d114bfc
pablovvoliveira <pablovvoliveira@gmail.com>
2023-06-25 11:17:46 -0300

    Completing client repository

--- src/main/java/com/pablovvoliveira/vendasapi/VendasApiApplication.java
+++ src/main/java/com/pablovvoliveira/vendasapi/VendasApiApplication.java
@@ -19,11 +19,37 @@ public class VendasApiApplication {
 	@Bean
 	public CommandLineRunner init(@Autowired Clientes clientes) {
 		return args -> {
+			System.out.println("Salvando clientes:");
 			clientes.salvar(new Cliente("Pablo"));
 			clientes.salvar(new Cliente("Tonho"));
 			
 			List<Cliente> todosClientes = clientes.obterTodos();
 			todosClientes.forEach(System.out::println);
+			
+			System.out.println();
+			System.out.println("Atualizando clientes...");
+			todosClientes.forEach(c -> {
+				c.setNome(c.getNome() + " atualizado");
+				clientes.atualizar(c);
+			});
+			
+			System.out.println();
+			System.out.println("Buscando clientes");
+			clientes.buscarPorNome("nho").forEach(System.out::print);
+			
+			System.out.println();
+			System.out.println();
+			System.out.println("Deletando clientes");
+			/*clientes.obterTodos().forEach(c -> {
+				clientes.deletar(c);
+			});*/
+			todosClientes = clientes.obterTodos();
+			if(todosClientes.isEmpty()) {
+				System.out.println("Nenhum cliente encontrado");
+			}
+			todosClientes.forEach(System.out::println);
+			
+			
 		};
 	}
 	
