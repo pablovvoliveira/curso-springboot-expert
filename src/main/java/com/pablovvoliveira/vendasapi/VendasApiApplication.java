7723c3f6d817f3be6146f587af64378b2ffd8fdb
pablovvoliveira <pablovvoliveira@gmail.com>
2023-06-26 09:49:02 -0300

    Mapping Produto

--- src/main/java/com/pablovvoliveira/vendasapi/VendasApiApplication.java
+++ src/main/java/com/pablovvoliveira/vendasapi/VendasApiApplication.java
@@ -35,7 +35,7 @@ public class VendasApiApplication {
 			
 			System.out.println();
 			System.out.println("Buscando clientes");
-			clientes.findByNomeLike("nho").forEach(System.out::print);
+			clientes.findByNomeLike("Pablo").forEach(System.out::print);
 			
 			System.out.println();
 			System.out.println();
