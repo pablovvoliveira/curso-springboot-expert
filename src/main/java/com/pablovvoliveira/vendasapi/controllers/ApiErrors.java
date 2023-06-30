3d6c7cc3f473ba123e4390ab73da13b7fcc44fbc
pablovvoliveira <pablovvoliveira@gmail.com>
2023-06-30 15:36:52 -0300

    ApplicationControllerAdvice and Exception handler

---
+++ src/main/java/com/pablovvoliveira/vendasapi/controllers/ApiErrors.java
@@ -0,0 +1,17 @@
+package com.pablovvoliveira.vendasapi.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.Getter;
+
+
+public class ApiErrors {
+	
+	@Getter
+	private List<String> errors;
+	
+	public ApiErrors(String msgErro) {
+		this.errors = Arrays.asList(msgErro);
+	}
+}
--- src/main/java/com/pablovvoliveira/vendasapi/controllers/PedidoController.java
+++ src/main/java/com/pablovvoliveira/vendasapi/controllers/PedidoController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,8 @@ import com.pablovvoliveira.vendasapi.DTO.PedidoDTO;
 import com.pablovvoliveira.vendasapi.entity.Pedido;
 import com.pablovvoliveira.vendasapi.services.PedidoService;
 
-@RestController("/api/pedidos")
+@RestController
+@RequestMapping("/api/pedidos")
 public class PedidoController {
 	
 	@Autowired
